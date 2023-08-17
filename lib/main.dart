4a5db1ab7c5160da40870ea8f4a778ac5df24598
Marvyoha <125772477+Marvyoha@users.noreply.github.com>
2023-08-17 11:23:49 +0100

    testing

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/home.dart';
+import 'package:to_do_app/themes.dart';
+
+void main() {
+  ThemeClass myTheme = ThemeClass();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: myTheme.theme,
+    home: const Home(),
+  ));
+}
